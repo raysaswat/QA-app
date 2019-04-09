a411c579e935e3ffcf75b41d4ef2d1e7d5e315de
Saswat Ray <ray.saswat2@gmail.com>
2019-04-09 19:35:27 +0530

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -11,6 +11,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    correct_ans = models.BooleanField()
     votes = models.IntegerField(default=0)
     
     def __str__(self):
